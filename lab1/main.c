bb1bc7d521f41b15177415000b6f07e424c2e680
Bel9sh <129883094+Bel9shik@users.noreply.github.com>
2023-09-13 22:13:44 +0700

    changed code

--- lab1/main.c
+++ lab1/main.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include <math.h>
 #include <stdio.h>
 
@@ -8,16 +7,12 @@ void calculate(int N) {
     for (int i = 0; i < N; i++) {
         pi += 4 * ((pow(-1,i)) / (2 * i + 1));
     }
-    printf("%lf", pi);
+    printf("%lf\n", pi);
 }
 
 int main() {
     int N;
-    printf("Print the count of repeats\n");
-    if (scanf("%d", &N) != 1) {
-        printf("incorrect input");
-        return 1;
-    }
+    N = 375000000;
     calculate(N);
     return 0;
 }
