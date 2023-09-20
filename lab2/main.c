49198aa3e199fe8eeeb146420ed3d3614aa2a4a5
Bel9sh <129883094+Bel9shik@users.noreply.github.com>
2023-09-20 19:36:24 +0700

    updated files

--- lab2/main.c
+++ lab2/main.c
@@ -1,1 +1,18 @@
+#include <math.h>
+#include <stdio.h>
 
+void calculate(int N) {
+    double pi;
+
+    for (int i = 0; i < N; i++) {
+        pi += 4 * ((pow(-1,i)) / (2 * i + 1));
+    }
+    printf("%lf\n", pi);
+}
+
+int main() {
+    int N;
+    N = 375000000;
+    calculate(N);
+    return 0;
+}
