a248ee03f005c4dcc52f1193639209c6aeb4e46c
Alexander Kardash <129883094+Bel9shik@users.noreply.github.com>
2023-10-12 19:22:11 +0700

    Update main.c

--- lab3/main.c
+++ lab3/main.c
@@ -1,11 +1,11 @@
-#include <math.h>
 #include <stdio.h>
 
 void calculate(int N) {
     double pi = 0;
-
+    double powNum;
     for (int i = 0; i < N; i++) {
-        pi += 4 * ((pow(-1,i)) / (2 * i + 1));
+        i % 2 == 0 ? (powNum = 1) : (powNum = -1);
+        pi += 4 * (powNum /  (2 * i + 1));
     }
     printf("%.9f\n", pi);
 }
