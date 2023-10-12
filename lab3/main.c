e494bea461ecedb126070aba386c3e15dfac711b
Alexander Kardash <129883094+Bel9shik@users.noreply.github.com>
2023-10-12 18:53:31 +0700

    Update main.c

--- lab3/main.c
+++ lab3/main.c
@@ -1,5 +1,18 @@
-#include <stdio>
+#include <math.h>
+#include <stdio.h>
+
+void calculate(int N) {
+    double pi = 0;
+
+    for (int i = 0; i < N; i++) {
+        pi += 4 * ((pow(-1,i)) / (2 * i + 1));
+    }
+    printf("%.9f\n", pi);
+}
 
 int main() {
-  return 0;
+    int N;
+    N = 800000000;
+    calculate(N);
+    return 0;
 }
